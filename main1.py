410d5f875b1408a6ed4edc11075baa6a8ca95b50
Joel Tia <joeltia225@gmail.com>
2026-04-02 05:24:33 -0400

    Solution d'un exercice sur Pdf

---
+++ main1.py
@@ -0,0 +1,40 @@
+from fpdf import FPDF
+import pandas as pd
+from fpdf.enums import XPos, YPos
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    # Set the header
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, text=row["Topic"], align="L",
+         new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
+
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, text=row["Topic"], align="R")
+
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, text=row["Topic"], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
+
+pdf.output("output.pdf")
